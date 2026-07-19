15615626f26f1864141319891bc5897b12aea680
Sukuna-05 <sahinaparwin1988@gmail.com>
2026-07-19 11:32:53 +0530

    Add dummy web server and update log messages

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const mineflayer = require('mineflayer');
+const http = require('http');
+
+// Dummy web server to satisfy Render's port requirement
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot is running online!\n');
+});
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+    console.log(`Dummy server listening on port ${PORT} to bypass Render scanner.`);
+});
 
 function createBot() {
     const bot = mineflayer.createBot({
@@ -9,7 +20,7 @@ function createBot() {
     });
 
     bot.on('spawn', () => {
-        console.log('Bot successfully spawned in the server.');
+        console.log('Bot successfully spawned in the BlitzKreig server.');
         setInterval(() => {
             bot.setControlState('jump', true);
             setTimeout(() => bot.setControlState('jump', false), 500);
@@ -17,11 +28,12 @@ function createBot() {
     });
 
     bot.on('end', () => {
-        console.log('Disconnected. Attempting to reconnect in 30 seconds...');
+        console.log('Disconnected from BlitzKreig. Reconnecting in 30 seconds...');
         setTimeout(() => createBot(), 30000);
     });
 
-    bot.on('error', (err) => console.log(`Error: ${err}`));
+    bot.on('error', (err) => console.log(`Network Error: ${err}`));
 }
 
 createBot();
+           
